f549d17eb943dc6832c0ccb4b365e93fcc58fe4b
sametegeli-oss <sametegeli@gmail.com>
2026-05-23 11:07:35 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='word-mode-pron-map-forced-v102';
+const CACHE_NAME='word-mode-clean-reallife-v104';
 const ASSETS=['./','./index.html','./sozluk.json','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim();});
